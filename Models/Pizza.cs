6551db6ed8d4f33bd63c03d2b88bd42a45e4ceaf
Christian <200390696@student.georgianc.on.ca>
2023-07-17 18:12:05 -0400

    Add decorators

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,4 +1,6 @@
-﻿namespace PizzaStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaStore.Models
 {
     
     public enum Cheese
@@ -27,11 +29,24 @@
     public class Pizza
     {
         int Id { get; set; }
+
+
+        [Required()]
+        [MaxLength(100)]
         String Name { get; set; }
+
+        [Required()]
+        [Range(0, 30000)] //Unsure how many calories are in an entire pizza, especially one loaded with toppings
         int Calories { get; set; } // Later, calculate this based on dough, cheese, and toppings
+
+        [Required()]
         Dough DoughType { get; set; }
-        Cheese CheeseType { get; set; }
+
+        Cheese CheeseType { get; set; } //Not required, as cheese is technically optional
+
+        [Required()]
         Boolean IsVegan { get; set; } //Later, determine this based on dough, cheese, and toppings
+
         List<Topping>? Toppings { get; set; } //Optional, toppings not required
     }
 }
--- Models/Topping.cs
+++ Models/Topping.cs
@@ -1,4 +1,6 @@
-﻿namespace PizzaStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaStore.Models
 {
     /**
      * Class for a Pizza Topping object
@@ -8,9 +10,21 @@
     public class Topping
     {
         int Id { get; set; }
+
+        [Required()]
+        [MaxLength(100)]
         String Name { get; set; }
+
+        [Required()]
+        [Range(0, 200)]
         Double Price { get; set; }
+
+        [Required()]
+        [Range(0, 3000)]
         int Calories { get; set; }
+
+        [Required()]
+        [Display(Name = "Vegan")]
         Boolean IsVegan { get;  set; }
     }
 }
