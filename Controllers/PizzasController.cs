3381ab76467f437aa583989be91658a22235f9de
chrisbarbati <113151356+chrisbarbati@users.noreply.github.com>
2023-11-15 21:00:05 -0500

    Revert "Merge branch 'master' into master2"
    This reverts commit 86501986871158993c2c7787e416d6eb99501eb4, reversing
    changes made to 7aeb4538b08c73a6b9cc23a1afb0383ed7a6572b.

--- Controllers/PizzasController.cs
+++ Controllers/PizzasController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -55,24 +54,6 @@ namespace PizzaStore.Controllers
         // GET: Pizzas/Create
         public IActionResult Create()
         {
-            //Get the data for toppings that are currently stored in the database, and add them to a new List of Toppings
-            List<Topping> Toppings = _context.Toppings.ToList();
-
-            //Create a new List of Images to hold all of the topping images
-            List<Image> ToppingImages = new List<Image>();
-
-            //Populate the list of topping images based on the toppings
-            //foreach(Topping topping in Toppings)
-            //{
-            //    ToppingImages.Add(Image.FromFile("/pizzaImages/" + topping.imagePath));
-            //}
-
-            System.Diagnostics.Debug.WriteLine("test");
-
-            //Add them to a ViewBag so the View can access them
-            ViewBag.Toppings = Toppings;
-            ViewBag.ToppingImages = ToppingImages;
-
             return View();
         }
 
@@ -121,17 +102,6 @@ namespace PizzaStore.Controllers
                 {//All other pizzas have crust1
                     pizza.IsGlutenFree = false;
                     pizza.ImagePath = "crust1.png";
-                }
-
-                /*
-                 * Code is not co-operating, use this for diagnostics and delete it later on
-                 * 
-                 * Problem: int[] array being passed is empty. 
-                 */
-
-                foreach (Topping topping in pizza.Toppings)
-                {
-                    System.Diagnostics.Debug.WriteLine(topping.Name);
                 }
 
                 _context.Add(pizza);
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PizzaStore.Models;
 using System.Reflection.Emit;
